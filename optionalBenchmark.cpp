339ec34c9d40c11e5c38fa898a77cdad2ec27f32
fenbf <bartlomiej.filipek@gmail.com>
2021-10-26 09:07:11 +0200

    initial

---
+++ optionalBenchmark.cpp
@@ -0,0 +1,136 @@
+#include <benchmark/benchmark.h>
+#include <optional>
+#include <string>
+#include "markable.hpp"
+
+using namespace ak_toolkit;
+
+
+using TNum = int;
+
+const TNum MAGIC = 7;
+const TNum ITERS = 10'000;
+
+typedef markable<mark_int<TNum, std::numeric_limits<TNum>::min()>> opt_compact_int;
+
+TNum get_int(TNum n) {
+    return n & 0xFF;
+}
+
+TNum sum_simple() {
+    TNum sum = 0;
+    for (TNum i = 0; i < ITERS; ++i) {
+        const auto n = get_int(i);
+        if (n != MAGIC)
+            sum += n;
+    }
+    return sum;
+}
+
+static void SumSimple(benchmark::State& state) {
+    // Code inside this loop is measured repeatedly
+    for (auto _ : state) {
+        auto s = sum_simple();
+        benchmark::DoNotOptimize(s);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(SumSimple);
+
+std::optional<TNum> get_optional_int(TNum n) {
+    const auto i = n & 0xFF;
+    return i != MAGIC ? std::optional<TNum>{i} : std::nullopt;
+}
+
+TNum sum_optional() {
+    TNum sum = 0;
+    for (TNum i = 0; i < ITERS; ++i) {
+        const auto n = get_optional_int(i);
+        if (n)
+            sum += n.value();
+    }
+    return sum;
+}
+
+static void SumOptional(benchmark::State& state) {
+    // Code inside this loop is measured repeatedly
+    for (auto _ : state) {
+        auto s = sum_optional();
+        benchmark::DoNotOptimize(s);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(SumOptional);
+
+opt_compact_int get_compact_optional_int(TNum n) {
+    const auto i = n & 0xFF;
+    return i != MAGIC ? opt_compact_int{ i } : opt_compact_int{};
+}
+
+TNum sum_compact_optional() {
+    TNum sum = 0;
+    for (TNum i = 0; i < ITERS; ++i) {
+        const auto n = get_compact_optional_int(i);
+        if (n.has_value())
+            sum += n.value();
+    }
+    return sum;
+}
+
+static void SumCompactOptional(benchmark::State& state) {
+    // Code inside this loop is measured repeatedly
+    for (auto _ : state) {
+        auto s = sum_compact_optional();
+        benchmark::DoNotOptimize(s);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(SumCompactOptional);
+
+static void CreateOptional(benchmark::State& state) {
+    // Code inside this loop is measured repeatedly
+    for (auto _ : state) {
+        std::optional<std::string> opt;
+        opt = "Hello World";
+        benchmark::DoNotOptimize(opt);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(CreateOptional);
+
+struct string_marked_value                           // a policy which defines the representaioion of the
+    : ak_toolkit::markable_type<std::string>           // 'marked' (special) std::string value
+{
+    static std::string marked_value() {                // create the marked value
+        return std::string("\0\0", 2);
+    }
+    static bool is_marked_value(const std::string& v) { // test if a given value is considered marked
+        return v.compare(0, v.npos, "\0\0", 2) == 0;
+    }
+};
+
+typedef ak_toolkit::markable<string_marked_value> opt_compact_str;
+
+
+static void CreateCompactOptional(benchmark::State& state) {
+    // Code inside this loop is measured repeatedly
+    for (auto _ : state) {
+        opt_compact_str opt("Hello World");
+        //opt.assign("Hello World");
+        benchmark::DoNotOptimize(opt);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(CreateCompactOptional);
+
+static void CreateString(benchmark::State& state) {
+    // Code inside this loop is measured repeatedly
+    for (auto _ : state) {
+        std::string str;
+        str = "Hello World";
+        benchmark::DoNotOptimize(str);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(CreateString);
+
